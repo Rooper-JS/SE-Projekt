5d932bbd354e7a6408f2e05951fa0983b51e05a8
Rooper1337 <julian.seiler@freenet.de>
2021-03-05 10:54:57 +0100

    Select hinzugefügt --> DozentenID wird nun aus der ausgewählten option ausgelesen

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -1,8 +1,16 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-const sql = require('mssql')
+var sql = require('mssql')
 var path = require('path');
+
+//DB-Config
+var config = {
+    user: 'wwwstdplan',
+    password: 'Key123',
+    server: 'localhost',
+    database: 'stundenplan'
+};
 
 //Main-Path
 var dir = __dirname;
@@ -13,11 +21,12 @@ app.get('/', function (req, res) {
    res.sendFile(path.join(dir + 'index.html'));
 });
 app.get('/getLessons', getLessons);
+app.get('/getDozenten', getDozenten)
 
 //statische Dateien bereitstellen
 app.use('/static', express.static(dir +'includes'));
 
-
+//__________________________________________________________________________________________________________________________________
 
 async function getLessons(req, res) {
     
@@ -39,17 +48,7 @@ async function getLessons(req, res) {
 function SQLgetLessons(ID) {
     return new Promise(function (resolve, reject) {
 
-
         var id = ID;
-        var sql = require("mssql");
-
-        // config for your database
-        var config = {
-            user: 'wwwstdplan',
-            password: 'Key123',
-            server: 'localhost',
-            database: 'stundenplan'
-        };
 
         // connect to your database
         sql.connect(config, function (err) {
@@ -73,12 +72,46 @@ function SQLgetLessons(ID) {
 
             });
         });
-
-
     });
 
 }
 
+async function getDozenten(req, res) {
+
+    //DB-query
+    var data = Promise.resolve(SQLgetDozenten());
+
+    function SQLgetDozenten() {
+        return new Promise(function (resolve, reject) {
+
+            sql.connect(config, function (err) {
+
+                if (err) console.log(err);
+
+                // create Request object
+                var request = new sql.Request();
+
+                // query to the database and get the records
+                request.query('select titel, vorname, name, DozentID, FachbereichID FROM dozent order by name, vorname, DozentID', function (err, recordset) {
+
+                    if (err) {
+                        console.log(err);
+                        reject(err);
+                    }
+
+                    // send records as a response
+                    resolve(recordset.recordset);
+
+                });
+            });
+        });
+
+    }
+    
+    res.send(await data);
+}
+
+//__________________________________________________________________________________________________________________________________
 
 var server = app.listen(8080, function () {
     var host = server.address().address;
